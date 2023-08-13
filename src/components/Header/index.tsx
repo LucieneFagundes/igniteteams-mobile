eb18cd723aad1563fcdb7d7757d8026e46257fdb
LucieneFagundes <lfnascimento99@gmail.com>
2023-08-12 21:44:48 -0300

    feat: :sparkles: add backbutton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
-export function Header() {
+type Props = {
+	showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
 	return (
 		<Container>
+			{showBackButton && (
+				<BackButton>
+					<BackIcon color="#FFF" size={32} />
+				</BackButton>
+			)}
 			<Logo source={logoImg} />
 		</Container>
 	);
