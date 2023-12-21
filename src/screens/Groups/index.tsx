a4ab3ee9f92aec9e16ba446278940e98da496d67
LucieneFagundes <lfnascimento99@gmail.com>
2023-12-20 22:08:10 -0300

    feat: add highlight and groupcard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
-import { Container,  } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
-	return (
-		<Container>
-			<Header />
-		</Container>
-	);
+  return (
+    <Container>
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera da FutureMe" />
+    </Container>
+  );
 }
