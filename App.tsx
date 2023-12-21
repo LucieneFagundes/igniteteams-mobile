52a62835a66ee33d6cd8f2023bbb4da3fbbb1144
LucieneFagundes <lfnascimento99@gmail.com>
2023-12-20 22:06:48 -0300

    chore: version of dependencies and formmating

--- App.tsx
+++ App.tsx
@@ -3,24 +3,24 @@ import theme from "./src/theme";
 
 import { Loading } from "@components/Loading";
 import {
-	Roboto_400Regular,
-	Roboto_700Bold,
-	useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
 } from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import { StatusBar } from "react-native";
 
 export default function App() {
-	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-	return (
-		<ThemeProvider theme={theme}>
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor="transparent"
-				translucent
-			/>
-			{fontsLoaded === true ? <Groups /> : <Loading />}
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded === true ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
